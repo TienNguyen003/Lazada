0f0a9fed38bb4de65f1afe9e56512a654b249672
Tien <dinhtien17082003@gmail.com>
2023-09-07 16:19:17 +0700

    Lazada

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,3 +1,4 @@
+/* eslint-disable eqeqeq */
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 
@@ -8,18 +9,28 @@ import styles from './login.module.scss';
 const cx = classNames.bind(styles);
 
 function Login() {
+    const handleClickLog = (e) => {
+        const ipName = document.querySelector('.login_ip-name__mZQtb').value;
+        const ipPass = document.querySelector('.login_ip-pass__5f2U7').value;
+
+        const itemName = localStorage.getItem('Name') ? JSON.parse(localStorage.getItem('Name')) : [];
+        const itemPass = localStorage.getItem('Passwork') ? JSON.parse(localStorage.getItem('Passwork')) : [];
+
+        for (let i = 0; i < itemName.length; i++) {
+            if (ipName == itemName[i] && ipPass == itemPass[i]) {
+                alert('Đăng nhập thành công');
+                window.location = 'http://localhost:3000/Lazada'
+            }
+        }
+    };
+
     return (
         <div className={cx('login')}>
             <div className={cx('intro')}>
-                <p className={cx('title')}>
-                    Chào mừng đến với Lazada. Đăng nhập ngay!
-                </p>
+                <p className={cx('title')}>Chào mừng đến với Lazada. Đăng nhập ngay!</p>
                 <p className={cx('transfer')}>
                     Thành viên mới?{' '}
-                    <Link
-                        to={routesConfig.Register}
-                        className={cx('trans-res')}
-                    >
+                    <Link to={routesConfig.Register} className={cx('trans-res')}>
                         Đăng kí
                     </Link>{' '}
                     tại đây
@@ -28,9 +39,7 @@ function Login() {
             <div className={cx('form-login')}>
                 <div className={cx('normally')}>
                     <div className={cx('user')}>
-                        <p className={cx('txtName')}>
-                            Số điện thoại hoặc email
-                        </p>
+                        <p className={cx('txtName')}>Số điện thoại hoặc email</p>
                         <input
                             className={cx('ip-name')}
                             type="text"
@@ -39,37 +48,23 @@ function Login() {
                     </div>
                     <div className={cx('pass')}>
                         <p className={cx('txtName')}>Mật khẩu</p>
-                        <input
-                            className={cx('ip-pass')}
-                            type="password"
-                            placeholder="Vui lòng nhập mật khẩu của bạn"
-                        />
+                        <input className={cx('ip-pass')} type="password" placeholder="Vui lòng nhập mật khẩu của bạn" />
                     </div>
                     <div className={cx('forgot-pass')}>
-                    <Link
-                        to={routesConfig.FamiliarPass}
-                        className={cx('forgot')}
-                    >
-                        Quên mật khẩu?
-                    </Link>
+                        <Link to={routesConfig.FamiliarPass} className={cx('forgot')}>
+                            Quên mật khẩu?
+                        </Link>
                     </div>
                 </div>
                 <div className={cx('third-party')}>
                     <ButtonCustom
                         className={cx('btn-log')}
                         children={'ĐĂNG NHẬP'}
+                        onClick={(e) => handleClickLog(e)}
                     ></ButtonCustom>
                     <p className={cx('txt-third')}>Hoặc, đăng nhập bằng</p>
-                    <ButtonCustom
-                        className={cx('fb')}
-                        href={'https://www.facebook.com/'}
-                        children={'Facebook'}
-                    />
-                    <ButtonCustom
-                        className={cx('gg')}
-                        href={'https://www.google.com/'}
-                        children={'Google'}
-                    />
+                    <ButtonCustom className={cx('fb')} href={'https://www.facebook.com/'} children={'Facebook'} />
+                    <ButtonCustom className={cx('gg')} href={'https://www.google.com/'} children={'Google'} />
                 </div>
             </div>
         </div>
--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,5 +1,7 @@
+/* eslint-disable eqeqeq */
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import routesConfig from '../../config/routes';
 import ButtonCustom from '../../component/layout/Button/button';
@@ -8,6 +10,59 @@ import styles from './register.module.scss';
 const cx = classNames.bind(styles);
 
 function Register() {
+    const itemName = localStorage.getItem('Name') ? JSON.parse(localStorage.getItem('Name')) : [];
+    const itemPass = localStorage.getItem('Passwork') ? JSON.parse(localStorage.getItem('Passwork')) : [];
+    const itemUser = localStorage.getItem('NameUser') ? JSON.parse(localStorage.getItem('NameUser')) : [];
+
+    const arr = [];
+
+    useEffect(() => {
+        itemName.forEach((item) => {
+            arr.push(item);
+        });
+    });
+
+    const handleClickRes = (e) => {
+        let check = [];
+        const resNameUser = document.querySelector('.register_ip-nameUser__oLMzD').value;
+        const resName = document.querySelector('.register_ip-name__WaBh8').value;
+        const resPass = document.querySelector('.register_ip-pass__kDBYK').value;
+
+        if (
+            resNameUser.trim() == '' ||
+            resName.trim() == '' ||
+            resName.length < 10 ||
+            resPass.trim() == '' ||
+            resPass.length < 6
+        ) {
+            alert('Tạo tài khoản thất bại');
+        } else {
+            arr.map((item) => {
+                if (resName == item) {
+                    alert('Tài khoản đã được đăng kí! Vui lòng thử lại');
+                    check.push(0);
+                } else {
+                    check.push(1);
+                }
+            });
+        }
+
+        let checkAcc = check.every(function (item) {
+            return item > 0;
+        });
+
+        if (checkAcc) {
+            alert('Tạo tài khoản thành công');
+            // them tai khoan
+            itemName.push(resName);
+            itemPass.push(resPass);
+            itemUser.push(resNameUser);
+            localStorage.setItem('Name', JSON.stringify(itemName));
+            localStorage.setItem('Passwork', JSON.stringify(itemPass));
+            localStorage.setItem('NameUser', JSON.stringify(itemUser));
+        }
+    };
+
     return (
         <div className={cx('register')}>
             <div className={cx('intro')}>
@@ -24,11 +79,7 @@ function Register() {
                 <div className={cx('normally')}>
                     <div className={cx('user')}>
                         <p className={cx('txtName')}>Họ Tên</p>
-                        <input
-                            className={cx('ip-name')}
-                            type="text"
-                            placeholder="Họ Tên"
-                        />
+                        <input className={cx('ip-nameUser')} type="text" placeholder="Họ Tên" />
                     </div>
                     <div className={cx('user')}>
                         <p className={cx('txtName')}>Số điện thoại</p>
@@ -50,28 +101,18 @@ function Register() {
                     </div>
                     <div className={cx('user')}>
                         <p className={cx('txtName')}>Ngày sinh</p>
-                        <input
-                            className={cx('ip-name')}
-                            type="text"
-                            placeholder="Nhập ngày tháng năm sinh của bạn"
-                        />
+                        <input className={cx('ip-name')} type="text" placeholder="Nhập ngày tháng năm sinh của bạn" />
                     </div>
                 </div>
                 <div className={cx('third-party')}>
                     <ButtonCustom
                         className={cx('btn-log')}
                         children={'ĐĂNG KÍ'}
+                        onClick={(e) => handleClickRes(e)}
                     ></ButtonCustom>
                     <div className={cx('check')}>
-                        <input
-                            className={cx('check-res')}
-                            type="checkbox"
-                            aria-checked="false"
-                            value="on"
-                        />
-                        <p className={cx('txtYes')}>
-                            Tôi muốn cập nhật thông tin ưu đãi qua SMS
-                        </p>
+                        <input className={cx('check-res')} type="checkbox" aria-checked="false" value="on" />
+                        <p className={cx('txtYes')}>Tôi muốn cập nhật thông tin ưu đãi qua SMS</p>
                     </div>
                     <div className={cx('policy')}>
                         <span>
@@ -93,27 +134,15 @@ function Register() {
                             >
                                 Chính Sách Bảo Mật của Lazada
                             </a>{' '}
-                            của Lazada, bao gồm quyền thu thập, sử dụng, và tiết
-                            lộ dữ liệu cá nhân của tôi theo pháp luật quy định.
+                            của Lazada, bao gồm quyền thu thập, sử dụng, và tiết lộ dữ liệu cá nhân của tôi theo pháp
+                            luật quy định.
                         </span>
                     </div>
                     <p className={cx('txt-third')}>Hoặc, đăng kí bằng</p>
-                    <ButtonCustom
-                        outline
-                        className={cx('email')}
-                        children={'Đăng ký bằng Email'}
-                    />
+                    <ButtonCustom outline className={cx('email')} children={'Đăng ký bằng Email'} />
                     <div className={cx('social-network')}>
-                        <ButtonCustom
-                            className={cx('fb')}
-                            href={'https://www.facebook.com/'}
-                            children={'Facebook'}
-                        />
-                        <ButtonCustom
-                            className={cx('gg')}
-                            href={'https://www.google.com/'}
-                            children={'Google'}
-                        />
+                        <ButtonCustom className={cx('fb')} href={'https://www.facebook.com/'} children={'Facebook'} />
+                        <ButtonCustom className={cx('gg')} href={'https://www.google.com/'} children={'Google'} />
                     </div>
                 </div>
             </div>
